e867596f2ae038e4c3a3f171d93e3bc89e239325
Aleksei Iurin <19alex85@gmail.com>
2022-04-18 12:45:34 +0300

    Version with sha2

--- digest.rb
+++ digest.rb
@@ -1,20 +1,23 @@
 require 'digest'
 
-puts "Введите слово или фразу для шифрования:"
-phrase = STDIN.gets.chomp
+puts 'Введите слово или фразу для шифрования:'
+string = STDIN.gets.chomp
 
-puts "Каким способом зашифровать:"
-puts "1. MD5"
-puts "2. SHA1"
+puts 'Каким способом зашифровать:'
+puts '1. MD5'
+puts '2. SHA1'
+puts '3. SHA2'
+encrypt_method = STDIN.gets.to_i
 
-answer = STDIN.gets.to_i
-
-until answer.between?(1, 2)
-  puts "Выберите 1 или 2"
-  answer = STDIN.gets.to_i
+until encrypt_method.between?(1, 3)
+  puts 'Выберите 1, 2 или 3'
+  encrypt_method = STDIN.gets.to_i
 end
 
-case answer
-when 1 then puts Digest::MD5.hexdigest(phrase)
-when 2 then puts Digest::SHA1.hexdigest(phrase)
+puts 'Вот что получилось:'
+
+case encrypt_method
+when 1 then puts Digest::MD5.hexdigest(string)
+when 2 then puts Digest::SHA1.hexdigest(string)
+when 3 then puts Digest::SHA2.hexdigest(string)
 end
